7d432a20714baa9f4e0b166fc55253cc7b10087d
Jerson reyes <u20232215585@usco.edu.co>
2024-02-14 15:55:03 -0500

    Probando git|

---
+++ src/holamundo/Holamundo.java
@@ -0,0 +1,17 @@
+
+package holamundo;
+
+/**
+ *
+ * @author Jerson Leandro Reyes 20232215585
+ */
+public class Holamundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hoal");
+    }
+    
+}
